8fba59a6d9bdf24b6b762fd819c0244a70ea5bb1
dz00te <dz00te@framassa.org>
2026-02-15 17:36:23 +0100

    - Update  to 1.4.0 - Add TRUST_PROXY and ENABLE_HSTS env vars for Traefik

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/postgres:17-alpine docker.io/redis:7-alpine ghcr.io/patchmon/patchmon-backend:1.3.7 ghcr.io/patchmon/patchmon-frontend:1.3.7" \
+    --label="org.nethserver.images=docker.io/postgres:17-alpine docker.io/redis:7-alpine ghcr.io/patchmon/patchmon-backend:1.4.0 ghcr.io/patchmon/patchmon-frontend:1.4.0" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
